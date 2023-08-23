d6fb066e8136e522730ffab722790e1f25414bf5
m2margaret <margaritanenarokova@gmail.com>
2023-08-24 01:02:49 +0300

    новые нужные шрифты уже готовы к использованию! и сделайте кнопки пожалуйста для перехода к экранам минимально простые, чтоб тестить свой экран можно было

--- app/src/main/java/com/example/mainlist/MainActivity2.kt
+++ app/src/main/java/com/example/mainlist/MainActivity2.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,4 +16,6 @@ class MainActivity2 : AppCompatActivity() {
         val intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
     }
+
+
 }
---
+++ app/src/main/java/com/example/mainlist/queue_editing.kt
@@ -0,0 +1,11 @@
+package com.example.mainlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class queue_editing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_queue_editing)
+    }
+}
