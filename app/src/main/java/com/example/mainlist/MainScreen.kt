09ca2af8f8ac8bbe57b697c6f4126cf2e1239038
itwasjoke <andreyvasiliev1204@gmail.com>
2023-09-13 22:25:03 +0300

    update recyclerview

--- app/src/main/java/com/example/mainlist/MainScreen.kt
+++ app/src/main/java/com/example/mainlist/MainScreen.kt
@@ -19,31 +19,22 @@ class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         val myJson = """
-{
-  "nickname": "Nikita Kadun",
-  "email": "kadunnikitacom@gmail.com",
-  "id": "1",
-  "workStatus": "Student",
-  "group": "2391",
-  "errorDetail": null
-}
-""".trimIndent()
+        [{
+          turnName: "Зачетная неделя",
+          turnDesc: "Конкретно четко"
+        },
+        {
+          turnName: "Хуйня",
+          turnDesc: "вау"
+        }]
+        """.trimIndent()
         var gson = Gson()
-        var mMineUserEntity = gson?.fromJson(myJson, MineUserEntity.MineUserInfo::class.java)
+        var responseTurns = gson?.fromJson(myJson, Array<Turn>::class.java)?.toList()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainscreen)
         val UserName = findViewById<TextView>(R.id.userNameMain)
         val Status = findViewById<TextView>(R.id.statusMain)
         val Group = findViewById<TextView>(R.id.groupMain)
-        if (mMineUserEntity != null) {
-            UserName.text =mMineUserEntity.nickname
-        }
-        if (mMineUserEntity != null) {
-            Status.text =mMineUserEntity.workStatus
-        }
-        if (mMineUserEntity != null) {
-            Group.text =mMineUserEntity.group
-        }
         val bcreateturn = findViewById<Button>(R.id.bCreateTurn)
         bcreateturn.setOnClickListener {
             val intent = Intent(this, ListOfParticipants::class.java)
@@ -56,47 +47,85 @@ class MainScreen : AppCompatActivity() {
             startActivity(intent);
         }
 
-        val recyclerView: RecyclerView = findViewById(R.id.rec)
+        val recyclerView: RecyclerView = findViewById(R.id.turnsRec)
         recyclerView.layoutManager = LinearLayoutManager(this)
         val turnAdapter = TurnAdapter(this)
         val turnList = mutableListOf<Turn>()
-        var turn = Turn("Name","Descre")
-//        turn.turnDescOut = "Описаниееее"
-        turnList.add(0,turn)
+        responseTurns?.forEach{
+            var turn = Turn(it.turnName, it.turnDesc)
+            turnList.add(0,turn)
+        }
         recyclerView.adapter = turnAdapter
         turnAdapter.setItems(turnList)
 
     }
 
-    private fun fillList(): List<String> {
-        val data = mutableListOf<String>()
-        (0..30).forEach { i -> data.add("$i element") }
-        return data
-    }
 
-    private fun getCatList(): List<Char> {
 
-        val jsonList =
-            """
-{
-  "nickname": "Nikita Kadun",
-  "email": "kadunnikitacom@gmail.com",
-  "id": "1",
-  "workStatus": "Student",
-  "group": [ "2391","2392","2370","2371"]
-  "errorDetail": null
-}
-"""
 
-        return jsonList.toList()
-    }
 
 
     fun CreateTurn(view : View){
         val intent = Intent(this, Activity_Mainqueue::class.java)
         startActivity(intent)
     }
+//    super.onCreate(savedInstanceState)
+//    setContentView(R.layout.activity_mainscreen)
+//    val myJson = """
+//        [{
+//          turnName: "Зачетная неделя",
+//          turnDesc: "Конкретно четко"
+//        },
+//        {
+//          turnName: "Хуйня",
+//          turnDesc: "вау"
+//        }]
+//        """.trimIndent()
+//    var gson = Gson()
+//    var mMineUserEntity = gson?.fromJson(myJson, Array<Turn>::class.java)?.toList()
+//
+//    val UserName = findViewById<TextView>(R.id.userNameMain)
+//    val Status = findViewById<TextView>(R.id.statusMain)
+//    val Group = findViewById<TextView>(R.id.groupMain)
+//    //        if (mMineUserEntity != null) {
+////            UserName.text =mMineUserEntity.nickname
+////        }
+////        if (mMineUserEntity != null) {
+////            Status.text =mMineUserEntity.workStatus
+////        }
+////        if (mMineUserEntity != null) {
+////            Group.text =mMineUserEntity.group
+////        }
+//    val bcreateturn = findViewById<Button>(R.id.bCreateTurn)
+//    bcreateturn.setOnClickListener {
+//        val intent = Intent(this, ListOfParticipants::class.java)
+//        startActivity(intent);
+//    }
+//
+//    val bExit = findViewById<ImageButton>(R.id.exitBtn)
+//    bExit.setOnClickListener {
+//        val intent = Intent(this, QueueEditing::class.java)
+//        startActivity(intent);
+//    }
+//
+//    val recyclerView: RecyclerView = findViewById(R.id.turnsRecView)
+//    recyclerView.layoutManager = LinearLayoutManager(this)
+//    val turnAdapter = TurnAdapter(this)
+//    val turnList = mutableListOf<Turn>()
+//    mMineUserEntity?.forEach{
+//        var turn = Turn(it.turnName, it.turnDesc)
+//        turnList.add(0,turn)
+//    }
+//    recyclerView.adapter = turnAdapter
+//    turnAdapter.setItems(turnList)
 
+//}
+
+
+//fun CreateTurn(view : View){
+//    val intent = Intent(this, Activity_Mainqueue::class.java)
+//    startActivity(intent)
+//}
 
 
 }
