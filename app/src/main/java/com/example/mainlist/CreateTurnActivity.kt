915cae94632df4b39d3c7538c719745c886d7abd
yuri <ur24.ivanov@yandex.ru>
2023-10-24 21:43:41 +0300

    lalala

--- app/src/main/java/com/example/mainlist/CreateTurnActivity.kt
+++ app/src/main/java/com/example/mainlist/CreateTurnActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mainlist
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.KeyEvent
 import android.view.View
 import android.widget.Button
@@ -23,9 +25,16 @@ class CreateTurnActivity : AppCompatActivity() {
         allowGroupsRec.adapter = allowGroupAdapter
         allowGroupAdapter.setItems(allowGroupList)
 
+
+        val nameTurn : EditText = findViewById(R.id.editText2)
+        val warningText : TextView = findViewById(R.id.textView5)
+
         val saveButton = findViewById<Button>(R.id.createBtnCreate)
         val cancelButton = findViewById<Button>(R.id.backBtnCreate)
         val warningText1 : TextView = findViewById(R.id.textView14)
+
+
+
         saveButton.setOnClickListener {
 //            val msg: String = nameMassage.text.toString()
 //            if (msg.trim().isEmpty()) {
@@ -44,10 +53,38 @@ class CreateTurnActivity : AppCompatActivity() {
             finish()
         }
 
+
+        nameTurn.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(nameTurn.length() != 0){
+                    warningText.visibility = View.GONE
+                }
+                else {
+                    warningText.visibility = View.VISIBLE
+                }
+            }
+        })
+
+
         allowEdit.setOnKeyListener(object : View.OnKeyListener {
             override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                if(allowEdit.length() < 4 && allowEdit.length() >= 1){
+                    warningText1.visibility = View.VISIBLE
+                }
+                else if(allowEdit.length() == 4 || allowEdit.length() == 0){
+                    warningText1.visibility = View.GONE
+                }
                 if (event.action == KeyEvent.ACTION_DOWN &&
-                    keyCode == KeyEvent.KEYCODE_ENTER
+                    keyCode == KeyEvent.KEYCODE_ENTER &&
+                    allowEdit.length() == 4
                 ) {
                     val s = allowEdit.text.toString()
                     val g = AllowGroup(0, s.toInt())
@@ -59,12 +96,8 @@ class CreateTurnActivity : AppCompatActivity() {
 
                     return true
                 }
-                if(allowEdit.length() < 4 && allowEdit.length() >= 1){
-                    warningText1.visibility = View.VISIBLE
-                }
-                else if(allowEdit.length() == 4 || allowEdit.length() == 0){
-                    warningText1.visibility = View.GONE
-                }
+
+
                 return false
             }
         })
